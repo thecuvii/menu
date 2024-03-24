d5aed040287fc81139b109a148b2c926b28764c2
Cuvii <i@cuvii.dev>
2024-03-24 22:40:30 +0800

    feat: 青椒肉丝

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import mdx from "@next/mdx";
+const withMDX = mdx();
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  experimental: {
+    mdxRs: true,
+    ppr: true,
+  },
+};
+
+export default withMDX(nextConfig);
